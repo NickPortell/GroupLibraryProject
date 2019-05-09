c42c17e1c5de8791c0726249ec751ca42ea51db6
Chris Phillips <you@example.com>
2019-05-09 13:29:36 -0400

    gee golly

--- GroupLibraryProject/Book.cs
+++ GroupLibraryProject/Book.cs
@@ -8,6 +8,46 @@ namespace GroupLibraryProject
 {
     class Book
     {
-        private string Title;
+        private string title;
+        private string author;
+        private DateTime dueDate;
+        private string type;
+        private string status;
+
+        public string Title
+        {
+            set { title = value; }
+            get { return title; }
+        }
+        public string Author
+        {
+            set { author = value; }
+            get { return author; }
+        }
+        public DateTime DueDate
+        {
+            set { dueDate = value; }
+            get { return dueDate; }
+        } 
+        public string Type
+        {
+            set { type = value; }
+            get { return type; }
+        }
+        public string Status
+        {
+            set { status = value; }
+            get { return status; }
+        }
+
+        public Book(string _title, string _author, DateTime _dueDate, string _type, string _status)
+        {
+            title = _title;
+            author = _author;
+            dueDate = _dueDate;
+            type = _type;
+            status = _status;
+        }
+
     }
 }
